4b47f1464184fc9f24c0b227f04ef37dd47a0d9e
Jared Loosli <jloosli@gmail.com>
2013-05-14 15:28:00 -0600

    Cleaned up view

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -5,7 +5,7 @@
  * Time: 2:39 PM
  * To change this template use File | Settings | File Templates.
  */
-app.controller('dialerController', function ($scope, $http, $cookies, directoryFactory) {
+app.controller('dialerController', function ($scope, $http, $cookies, $filter, directoryFactory) {
 
     var storage = chrome.storage.sync;
 
@@ -17,15 +17,25 @@ app.controller('dialerController', function ($scope, $http, $cookies, directoryF
 
     $scope.directory = directoryFactory.getDirectory();
 
+//    $scope.$watch('query', function(newVal,oldVal) {
+//        $scope.filteredQuery = $filter('filter')($scope.directory, $scope.query);
+//        console.log($scope.query);
+//        console.log($scope.filteredQuery);
+//
+//    });
 
-    $scope.dialNumber = function () {
+
+    $scope.dialNumber = function (phoneNumber) {
         console.log("Dialing " + phoneNumber);
         $http({
             method: 'GET',
             url: 'https://dashboard.vocalocity.com/presence/rest/clicktocall/' + phoneNumber
         })
             .success(function (data, status, headers, config) {
-                console.log("Dialed " + phoneNumber + " successfully.");
+                $scope.alertMessage= "Dialed " + phoneNumber + " successfully.";
+                setTimeout(function() {
+                    $scope.alertMessage = '';
+                },2000)
             })
             .error(function (data, status, headers, config) {
                 console.log("Dialer error")
